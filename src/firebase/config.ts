20359ba79ea22051b1493d4ccc525106189122dc
Matteo Piantoni <piantonimatteo.99@gmail.com>
2025-12-22 16:02:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ac-chiari-import-2024",
+  "appId": "1:901135690459:web:bf13c0ff2d857b991c7afe",
+  "apiKey": "AIzaSyBg5bQxU4Lk0QDFkPb4zGc7686ax0rY-as",
+  "authDomain": "ac-chiari-import-2024.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "901135690459"
+};
