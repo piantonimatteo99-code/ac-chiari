025eca6fce5f42f2e3672ff4c0fd92140649e96d
Matteo Piantoni <piantonimatteo.99@gmail.com>
2025-12-23 23:37:51 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Missing

--- src/components/dati-utente-card.tsx
+++ src/components/dati-utente-card.tsx
@@ -117,7 +117,7 @@ export default function DatiUtenteCard() {
       <CardHeader>
         <CardTitle>I Miei Dati</CardTitle>
         <CardDescription>
-          Modifica i tuoi dati anagrafici. L'indirizzo qui specificato può essere usato come predefinito per i nuovi familiari.
+          Modifica i tuoi dati anagrafici. Questo indirizzo può essere usato come predefinito per i nuovi familiari.
         </CardDescription>
       </CardHeader>
       <CardContent>
