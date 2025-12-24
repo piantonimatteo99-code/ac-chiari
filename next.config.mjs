696a0c064dfef53a898874f9faaa69f9bf315586
Matteo Piantoni <piantonimatteo.99@gmail.com>
2025-12-24 08:45:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ['*.cloudworkstations.dev'],
+  },
+};
 
 export default nextConfig;
